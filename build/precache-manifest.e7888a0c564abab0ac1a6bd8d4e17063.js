00308a9f04bdff98bb2f6eaa2ec1dc3d1b375f88
luantafarel <luantafarel7@gmail.com>
2020-02-17 22:11:50 -0300

    Add files to github pages

---
+++ build/precache-manifest.e7888a0c564abab0ac1a6bd8d4e17063.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d4bf79c7fe25e02144ca3c8028d7945",
+    "url": "/monstersReact/index.html"
+  },
+  {
+    "revision": "427fded0b0b2f7b08542",
+    "url": "/monstersReact/static/css/main.54d84da1.chunk.css"
+  },
+  {
+    "revision": "b235cfb64ee3d31b3962",
+    "url": "/monstersReact/static/js/2.dfd295bd.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monstersReact/static/js/2.dfd295bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "427fded0b0b2f7b08542",
+    "url": "/monstersReact/static/js/main.9f7b528f.chunk.js"
+  },
+  {
+    "revision": "8bc9868cfd845759c4e0",
+    "url": "/monstersReact/static/js/runtime-main.6380d2a1.js"
+  }
+]);
